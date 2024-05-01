e3bcac4468406616bb0ec00871ef4e4f3ee40821
Donovan Glover <donovan@dglover.co>
2024-05-01 17:26:01 -0400

    meta: Explicitly declare async for onRequest.addListener

--- src/background.ts
+++ src/background.ts
@@ -26,8 +26,17 @@ browser.contextualIdentities.query({}).then(async contexts => {
   console.log(err)
 })
 
-// Listen for requests and proxy based on container
-browser.proxy.onRequest.addListener(async requestInfo => {
+/** Listen for requests and proxy based on container
+ *
+ * Note: Below only works with an async function, so it's possible that
+ * the upstream types are wrong.
+ *
+ * <https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/proxy/onRequest#addlistener_syntax>
+ * <https://bugzilla.mozilla.org/show_bug.cgi?id=1409878>
+ * <https://hg.mozilla.org/mozilla-central/rev/9f0ee2f582a2>
+ * <https://github.com/DefinitelyTyped/DefinitelyTyped/blob/89bf9961959cf/types/firefox-webext-browser/index.d.ts#L4290-L4294>
+ */
+browser.proxy.onRequest.addListener(async requestInfo => { // eslint-disable-line @typescript-eslint/no-misused-promises
   const id = requestInfo.cookieStoreId
 
   if (id === undefined || id === 'firefox-default') {
