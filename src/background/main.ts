155c7824dd55dc8039134c3520813d8e2469f072
Donovan Glover <donovan@dglover.co>
2024-05-01 07:16:19 -0400

    chore: Remove top-level await
    Also fixed the import location.

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,31 +1,31 @@
-import { MULLVAD_PUBLIC_API_URL, type MullvadServer } from './providers/Mullvad'
+import { MULLVAD_PUBLIC_API_URL, type MullvadServer } from '../providers/Mullvad'
 
 const browser = window.browser
 const colors = ['blue', 'turquoise', 'green', 'yellow', 'orange', 'red', 'pink', 'purple', 'toolbar']
 
-await Promise.all([
-  // Remove all existing containers
-  browser.contextualIdentities.query({}).then(async contexts => {
-    for (const context of contexts) {
-      await browser.contextualIdentities.remove(context.cookieStoreId)
+// Remove all existing containers
+browser.contextualIdentities.query({}).then(async contexts => {
+  for (const context of contexts) {
+    await browser.contextualIdentities.remove(context.cookieStoreId)
+  }
+
+  // Add containers based on Mullvad API
+  const servers: MullvadServer[] = await (await fetch(MULLVAD_PUBLIC_API_URL)).json()
+
+  for (const [i, server] of servers.entries()) {
+    const socks = server.socks_name
+
+    if (socks !== undefined) {
+      await browser.contextualIdentities.create({
+        name: socks,
+        color: colors[i % colors.length],
+        icon: 'circle'
+      })
     }
-
-    // Add containers based on Mullvad API
-    const servers: MullvadServer[] = await (await fetch(MULLVAD_PUBLIC_API_URL)).json()
-
-    for (const [i, server] of servers.entries()) {
-      const socks = server.socks_name
-
-      if (socks !== undefined) {
-        await browser.contextualIdentities.create({
-          name: socks,
-          color: colors[i % colors.length],
-          icon: 'circle'
-        })
-      }
-    }
-  })
-])
+  }
+}).catch(err => {
+  console.log(err)
+})
 
 // Listen for requests and proxy based on container
 browser.proxy.onRequest.addListener(requestInfo => {
