17fcb3d2c41dbc3b24b9c355a2893605219c45b6
Donovan Glover <donovan@dglover.co>
2024-05-01 13:40:00 -0400

    refactor: Clean up background script
    This works now that SWC transpiles things properly.

--- src/background/main.ts
+++ src/background/main.ts
@@ -1,39 +1,45 @@
+import { type MullvadServer } from '../providers/Mullvad'
+
 const colors = ['blue', 'turquoise', 'green', 'yellow', 'orange', 'red', 'pink', 'purple', 'toolbar']
-let i = 0
 
 // Remove all existing containers
 browser.contextualIdentities.query({}).then(async contexts => {
   for (const context of contexts) {
-    browser.contextualIdentities.remove(context.cookieStoreId)
+    await browser.contextualIdentities.remove(context.cookieStoreId)
   }
 
   // Add containers based on Mullvad API
-  const servers = await (await fetch('https://api-www.mullvad.net/www/relays/all/')).json()
+  const servers: MullvadServer[] = await (await fetch('https://api-www.mullvad.net/www/relays/all/')).json()
 
-  servers.forEach(server => {
+  for (const [i, server] of servers.entries()) {
     const socks = server.socks_name
-    console.log(server)
-    if (socks) {
-      browser.contextualIdentities.create({
+
+    if (socks !== undefined) {
+      await browser.contextualIdentities.create({
         name: socks,
-        color: colors[i++],
+        color: colors[i % colors.length],
         icon: 'circle'
       })
-
-      if (i === colors.length) i = 0
     }
-  })
+  }
+}).catch(err => {
+  console.log(err)
 })
 
 // Listen for requests and proxy based on container
 browser.proxy.onRequest.addListener(async requestInfo => {
   const id = requestInfo.cookieStoreId
-  if (id === 'firefox-default') return
+
+  if (id === undefined || id === 'firefox-default') {
+    return
+  }
 
   return {
     type: 'socks',
     host: await browser.contextualIdentities.get(id).then(context => {
       return context.name
+    }).catch(err => {
+      console.log(err)
     }),
 
     proxyDNS: true,
