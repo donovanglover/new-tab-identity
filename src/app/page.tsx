6beed27c59245acec74f7150759bc3e156be4c6c
Donovan Glover <donovan@dglover.co>
2024-05-02 15:07:47 -0400

    meta: Add working identities query inside component
    Will expand upon this later.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
 export default function RootPage (): React.ReactElement {
+  const status = browser.contextualIdentities !== undefined ? 'All good to go.' : 'browser.contextualIdentities not available.'
+
+  const eventHandler = (event: React.MouseEvent<HTMLButtonElement>): void => {
+    if (event.currentTarget.value === 'create') {
+      browser.tabs.create({
+        url: 'about:blank',
+        cookieStoreId: '1'
+      }).then().catch(e => { console.log(e) })
+    }
+  }
+
+  browser.contextualIdentities.query({})
+    .then((identities) => {
+      console.log(identities)
+
+      if (identities.length === 0) {
+        console.log('No identities returned from the API.')
+      }
+    }).catch(err => {
+      console.warn(err)
+    })
+
   return (
-    <div className="w-96">
-      <h1 className='text-red-100'>Vite + React</h1>
+    <div className="p-4">
+      <p>{status}</p>
+      <button className='text-center text-4xl font-bold' onClick={eventHandler}>New Tab Identity</button>
     </div>
   )
 }
