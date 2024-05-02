d11107f82ffe5e0fe7470d2b4a93241aca5859ea
Donovan Glover <donovan@dglover.co>
2024-05-02 08:47:58 -0400

    vite: Remove @vitejs/plugin-react-swc
    Not needed since we're developing the extension directly inside the web
    browser and using vite for its bundling.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'node:path'
-import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-
   root: 'src',
 
   build: {
