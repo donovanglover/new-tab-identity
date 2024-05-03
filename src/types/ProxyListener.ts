41baf5c60ec56ed78eb6c9ac9b2c53e7626f600a
Donovan Glover <donovan@dglover.co>
2024-05-03 19:11:31 -0400

    meta: Use .ts files for types
    Seems to help with type checking from the LSP.

--- src/types/ProxyListener.ts
+++ src/types/ProxyListener.ts
@@ -1,3 +1,3 @@
-import type { ProxyInfo } from './ProxyInfo'
+import type { ProxyInfo } from './ProxyInfo.ts'
 
 export type ProxyListener = (requestInfo: browser.proxy._OnRequestDetails) => Promise<ProxyInfo>
