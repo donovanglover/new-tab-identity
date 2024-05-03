657983f9ea637c4a644a36321731867baf08a9ff
Donovan Glover <donovan@dglover.co>
2024-05-03 19:43:35 -0400

    lib: Add BrowserContainers class

--- src/components/UpdateServerList.tsx
+++ src/components/UpdateServerList.tsx
@@ -1,25 +1,20 @@
-import { ContainerList } from '../lib/browser'
+import { BrowserContainers } from '../lib/BrowserContainers'
 import { fetchJSON } from '../lib/fetchJSON'
 import { MULLVAD_PUBLIC_API_URL } from '../lib/globals'
 import type { MullvadServer } from '../types/MullvadServer'
 import Button from './Button'
 
 async function updateServerList (): Promise<void> {
-  const list = new ContainerList()
+  const containers = new BrowserContainers()
   const servers: MullvadServer[] = await fetchJSON(MULLVAD_PUBLIC_API_URL)
-
-  console.log(list)
-  console.log(servers)
 
   for (const server of servers) {
     const socks = server.socks_name
 
     if (socks !== undefined) {
-      await list.add(socks)
+      await containers.add(socks)
     }
   }
-
-  console.log(list)
 }
 
 export default function UpdateServerList (): React.ReactElement {
