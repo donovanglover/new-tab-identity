b09c6620bec851f3edad9ab9a26cba5c6643b918
Donovan Glover <donovan@dglover.co>
2024-05-03 18:45:37 -0400

    lib: Add fetchJSON()

---
+++ src/lib/fetchJSON.ts
@@ -0,0 +1,6 @@
+export async function fetchJSON (url: string): Promise<any> {
+  const response = await fetch(url)
+  const json = await response.json()
+
+  return json
+}
---
+++ tests/fetchJSON.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest'
+import { fetchJSON } from '../src/lib/fetchJSON'
+import { MULLVAD_PUBLIC_API_URL } from '../src/lib/Mullvad'
+
+describe('fetchJSON()', () => {
+  it('should fetch the mullvad api successfully without authentication', async () => {
+    expect(await fetchJSON(MULLVAD_PUBLIC_API_URL)).toBeDefined()
+  })
+})
