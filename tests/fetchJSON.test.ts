a878371fafa30a5f78c5ffb069dd9e0040f91dfd
Donovan Glover <donovan@dglover.co>
2024-05-03 20:29:00 -0400

    chore: Fix old mullvad url import

--- tests/fetchJSON.test.ts
+++ tests/fetchJSON.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import { fetchJSON } from '../src/lib/fetchJSON'
-import { MULLVAD_PUBLIC_API_URL } from '../src/lib/Mullvad'
+import { MULLVAD_PUBLIC_API_URL } from '../src/lib/globals'
 
 describe('fetchJSON()', () => {
   it('should fetch the mullvad api successfully without authentication', async () => {
