2432fdc3a2fc2b4324362078e81695bde1966c4e
Donovan Glover <donovan@dglover.co>
2024-05-03 20:18:01 -0400

    feat: Use useEffect to list all containers in popup
    Also made the popup scrollable for all the items.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,31 @@
+import { useEffect, useState } from 'react'
 import UpdateServerList from '../components/UpdateServerList'
 
 export default function RootPage (): React.ReactElement {
-  const status = browser.contextualIdentities !== undefined ? 'All good to go.' : 'browser.contextualIdentities not available.'
+  const [containers, setContainers] = useState<browser.contextualIdentities.ContextualIdentity[]>()
 
-  const eventHandler = (event: React.MouseEvent<HTMLButtonElement>): void => {
-    if (event.currentTarget.value === 'create') {
-      browser.tabs.create({
-        url: 'about:blank',
-        cookieStoreId: '1'
-      }).then().catch(e => { console.log(e) })
-    }
-  }
-
-  browser.contextualIdentities.query({})
-    .then((identities) => {
-      console.log(identities)
-
-      if (identities.length === 0) {
-        console.log('No identities returned from the API.')
-      }
+  useEffect(() => {
+    browser.contextualIdentities.query({}).then((containers) => {
+      setContainers(containers)
     }).catch(err => {
       console.warn(err)
     })
+  }, [containers])
 
   return (
     <div className="p-4">
       <h1 className='text-center text-4xl font-bold'>New Tab Identity</h1>
+
       <UpdateServerList />
-      <p>{status}</p>
+
+      {browser.contextualIdentities === undefined
+        ? <p>Go to <code>about:config</code> and set <code>privacy.userContext.enabled</code> to <code>true</code>, then restart your browser.</p>
+        : <p>All good to go.</p>
+      }
+
+      {containers?.map(container => {
+        return <p key={container.cookieStoreId}>{container.name}</p>
+      })}
     </div>
   )
 }
