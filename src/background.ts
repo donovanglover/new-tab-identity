8d9f2ca227036a76f541c5d84a50c9564cc4fff9
Donovan Glover <donovan@dglover.co>
2024-05-06 18:27:03 -0400

    refactor: Move event listeners to background script
    There's really no need to have a separate class constructor especially
    when we can change the functionality of an existing listener with the
    storage API for browser extensions.

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,50 @@
-import { BrowserProxy } from './lib/BrowserProxy'
+/* eslint-disable @typescript-eslint/no-misused-promises */
 
-const proxy = new BrowserProxy()
+const blockDefault = false
 
-proxy.start()
+browser.proxy.onRequest.addListener(
+  async requestInfo => {
+    const containerId = requestInfo.cookieStoreId
+
+    if (containerId === undefined) {
+      console.warn('Got unknown container, which should never happen.')
+
+      return {
+        type: 'direct'
+      }
+    }
+
+    if (containerId === 'firefox-default') {
+      return blockDefault
+        ? {
+            type: 'http',
+            host: '127.0.0.1',
+            port: 65535
+          }
+        : {
+            type: 'direct'
+          }
+    }
+
+    const context = await browser.contextualIdentities.get(containerId)
+    console.log(context)
+
+    return {
+      type: 'socks',
+      host: context.name,
+      proxyDNS: true,
+      port: 1080
+    }
+  },
+  {
+    urls: ['<all_urls>']
+  }
+)
+
+browser.proxy.onError.addListener(error => {
+  console.error(`new-tab-identity(proxy-error): ${error.message}`)
+})
+
+browser.tabs.onCreated.addListener(tab => {
+  console.log(tab)
+})
--- tests/BrowserProxy.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest'
-import { BrowserProxy } from '../src/lib/BrowserProxy'
-
-describe('BrowserProxy', () => {
-  it('should create a new proxy', () => {
-    expect(new BrowserProxy()).toBeDefined()
-  })
-})
