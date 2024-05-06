2aff5cad72d6d8448f37bba9dd030323201d7913
Donovan Glover <donovan@dglover.co>
2024-05-06 01:07:52 -0400

    chore: Merge all addon logic into single component
    Might be easier to manage this way with library stuff abstracted into
    lib.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
 import { useEffect, useState } from 'react'
-import UpdateServerList from '../components/UpdateServerList'
+import Button from '../components/Button'
+import { BrowserContainers } from '../lib/BrowserContainers'
+import { fetchJSON } from '../lib/fetchJSON'
+import { MULLVAD_PUBLIC_API_URL } from '../lib/globals'
+import type { MullvadServer } from '../types/MullvadServer'
+
+function updateServerList (): void {
+  const containers = new BrowserContainers()
+
+  fetchJSON(MULLVAD_PUBLIC_API_URL).then(result => {
+    const servers: MullvadServer[] = result
+
+    for (const server of servers) {
+      const socks = server.socks_name
+
+      if (socks !== undefined) {
+        containers.add(socks).then(result => {
+          console.log(result)
+        }).catch(e => {
+          console.warn(e)
+        })
+      }
+    }
+  }).catch(e => {
+    console.warn(e)
+  })
+}
 
 export default function RootPage (): React.ReactElement {
   const [containers, setContainers] = useState<browser.contextualIdentities.ContextualIdentity[]>()
@@ -12,11 +38,13 @@ export default function RootPage (): React.ReactElement {
     })
   }, [containers])
 
+  function deleteContainer (): void {}
+
   return (
     <div className="p-4">
       <h1 className='text-center text-4xl font-bold'>New Tab Identity</h1>
 
-      <UpdateServerList />
+      <Button onClick={updateServerList}>Update server list</Button>
 
       {browser.contextualIdentities === undefined
         ? <p>Go to <code>about:config</code> and set <code>privacy.userContext.enabled</code> to <code>true</code>, then restart your browser.</p>
@@ -24,7 +52,14 @@ export default function RootPage (): React.ReactElement {
       }
 
       {containers?.map(container => {
-        return <p key={container.cookieStoreId}>{container.name}</p>
+        return (
+          <p style={{ color: container.colorCode }} key={container.cookieStoreId}>
+            <span>({container.cookieStoreId.replace('firefox-container-', '')})</span>
+            {' '}
+            {container.name}
+            <Button onClick={deleteContainer}>Delete</Button>
+          </p>
+        )
       })}
     </div>
   )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,6 +8,6 @@ export interface ButtonProps {
 
 export default function Button ({ children, onClick, className }: ButtonProps): React.ReactElement {
   return (
-    <button className={clsx('rounded-md border-2 border-300 bg-200 px-4 py-2 shadow transition-colors hover:bg-300', className)} onClick={onClick}>{children}</button>
+    <button className={clsx('rounded-md border-2 border-300 bg-200 px-2 py-1 mx-2 shadow transition-colors hover:bg-300', className)} onClick={onClick}>{children}</button>
   )
 }
