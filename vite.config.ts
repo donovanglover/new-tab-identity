3619e1c7d21206dac9881879e6d611921eac61ac
Donovan Glover <donovan@dglover.co>
2024-05-08 06:30:42 -0400

    vitest: Add test coverage for components directory

--- vite.config.ts
+++ vite.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
     coverage: {
       enabled: true,
       provider: 'istanbul',
-      include: ['lib/**'],
+      include: ['lib/**', 'components/**'],
 
       reporter: ['text'],
       reportsDirectory: '../dist',
