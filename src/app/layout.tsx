3d5da18294d02c27313babe8512eada993e1160c
Donovan Glover <donovan@dglover.co>
2024-05-07 21:33:36 -0400

    style: Switch color scheme back to monokai

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const root = document.getElementById('root')
 if (root !== null) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
-      <div className="w-[64rem] max-w-full bg-100 text-700 base16-rose-pine-moon">
+      <div className="w-[64rem] max-w-full bg-100 text-700 base16-monokai">
         <RootPage />
       </div>
     </React.StrictMode>
