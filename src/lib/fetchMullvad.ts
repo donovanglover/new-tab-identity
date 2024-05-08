e28165d9e8ee601d554e96f4626915b15c05446a
Donovan Glover <donovan@dglover.co>
2024-05-07 20:39:01 -0400

    feat: Shuffle servers from fetchMullvad()
    This makes it possible to open a new randomized server every time we
    open a server from a certain location by iterating through the list.
    
    This also guarantees that the same server won't be used twice when
    opening a random server from a given location.

--- src/lib/fetchMullvad.ts
+++ src/lib/fetchMullvad.ts
@@ -2,7 +2,12 @@ import { type IMullvadServerWireguard, isWireguard, type MullvadServer } from '.
 
 /** The publicly accessible API endpoint used to fetch all Mullvad VPN servers.
  *
+ * Note that the list only includes `active` servers so users won't have the
+ * option to choose a server that doesn't work.
+ *
  * Used for <https://mullvad.net/servers>.
+ *
+ * @returns A **randomized** list of `active` Wireguard Mullvad servers
  */
 export async function fetchMullvad (): Promise<IMullvadServerWireguard[]> {
   const response = await fetch('https://api-www.mullvad.net/www/relays/all')
@@ -10,5 +15,22 @@ export async function fetchMullvad (): Promise<IMullvadServerWireguard[]> {
   const wireguard = json.filter(isWireguard)
   const active = wireguard.filter(wireguard => wireguard.active)
 
+  shuffle(active)
+
   return active
+}
+
+/** Shuffle an array unbiasedly.
+ *
+ * See: <https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle>
+ */
+function shuffle (array: any[]): void {
+  let currentIndex = array.length
+
+  while (currentIndex !== 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+  }
 }
