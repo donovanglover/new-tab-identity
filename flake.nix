c2cb4c0f9f0cabbc6d916779bb2cc7e51c7a1d17
Donovan Glover <donovan@dglover.co>
2024-08-19 02:08:44 -0400

    chore: Simplify flake

--- flake.nix
+++ flake.nix
@@ -5,15 +5,14 @@
 
   outputs = { nixpkgs, ... }: let
     inherit (nixpkgs.legacyPackages.${system}) pkgs;
-    inherit (pkgs) mkShell;
-    inherit (builtins) attrValues;
 
     system = "x86_64-linux";
   in {
-    devShells.${system}.default = mkShell {
-      nativeBuildInputs = attrValues {
-        inherit (pkgs) firefox nodejs;
-      };
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        firefox
+        nodejs
+      ];
     };
   };
 }
